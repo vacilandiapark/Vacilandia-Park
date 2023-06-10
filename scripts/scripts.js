2fed245466d60a60758c723007576e447a09b6bf
Michael <michaeld@vacilandia.com.pe>
2023-06-10 10:00:05 -0500

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,41 @@
-// Elimina el código JavaScript anterior relacionado con el evento click
-// ...
+// Obtén una referencia a los enlaces del menú de navegación
+const navLinks = document.querySelectorAll('.navbar-custom .navbar-nav .nav-link');
 
-// Agrega un evento hover a cada botón
+// Agrega un evento click a cada enlace
 navLinks.forEach(navLink => {
-    navLink.addEventListener('mouseenter', () => {
-      navLink.classList.add('bounce');
-    });
-  
-    navLink.addEventListener('mouseleave', () => {
-      navLink.classList.remove('bounce');
-    });
+  navLink.addEventListener('click', (event) => {
+    event.preventDefault(); // Evita que el enlace recargue la página
+    const href = navLink.getAttribute('href');
+    if (href) {
+      window.location.href = href; // Redirecciona a la URL especificada por el enlace
+    }
   });
+  
+  navLink.addEventListener('mouseenter', () => {
+    navLink.classList.add('bounce');
+  });
+
+  navLink.addEventListener('mouseleave', () => {
+    navLink.classList.remove('bounce');
+  });
+});
+
+
+const areas = document.querySelectorAll('map[name="mapa"] area');
+
+areas.forEach(area => {
+  area.addEventListener('mouseenter', () => {
+    document.body.style.cursor = 'pointer';
+  });
+
+  area.addEventListener('mouseleave', () => {
+    document.body.style.cursor = 'default';
+  });
+
+  area.addEventListener('click', () => {
+    const href = area.getAttribute('href');
+    if (href) {
+      window.location.href = href;
+    }
+  });
+});
