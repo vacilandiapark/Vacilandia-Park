61a7eb1f571488cbc22f5875b16c159392725a6b
Vacilandia Park <michaeld@vacilandia.com.pe>
2024-03-12 11:13:15 -0500

    update

--- script.js
+++ script.js
@@ -46,37 +46,29 @@ $(document).ready(function (ripples) {
 
 /* CÓDIGO PARA SCRIPTS QUE INTERFIEREN CON LA ANIMACIÓN DE BARBA.JS SE UNIFICA TODO*/
 
+// CÓDIGO PARA LA HAMBURGUESA DEL NAVBAR EN RESPONSIVE
+function initializeHamburguer() {
+  let hamburguer = document.querySelector(".hamburguer");
+  let responsive_navegacion = document.getElementById("responsive_navegacion");
+  let closeButton = document.querySelector("#responsive_navegacion .close-button");
+
+  hamburguer.addEventListener("click", function () {
+    responsive_navegacion.classList.add("open");
+  });
+
+  closeButton.addEventListener("click", function () {
+    responsive_navegacion.classList.remove("open");
+  });
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
-  let hamburguer;
-  let responsive_navegacion;
-  let closeButton;
-  let faqItems;
-  let modal;
-  let modalContent;
-  let closeModal;
-
-  /* CÓDIGO PARA LA HAMBURGUESA DEL NAVBAR EN RESPONSIVE */
-
-  function initializeHamburguer() {
-    hamburguer = document.querySelector(".hamburguer");
-    responsive_navegacion = document.getElementById("responsive_navegacion");
-    closeButton = document.querySelector("#responsive_navegacion .close-button");
-
-    hamburguer.addEventListener("click", function () {
-      responsive_navegacion.classList.add("open");
-    });
-
-    closeButton.addEventListener("click", function () {
-      responsive_navegacion.classList.remove("open");
-    });
-  }
-
-  /* CÓDIGO PARA LA AMPLIACIÓN DE IMAGEN EN MODO MODAL */
-
+  // CÓDIGO PARA LA AMPLIACIÓN DE IMAGEN EN MODO MODAL
   function initializeModal() {
-    modal = document.getElementById("modal");
-    modalContent = document.getElementById("modal-img");
-    closeModal = document.getElementById("close-modal");
+    let modal = document.getElementById("modal");
+    let modalContent = document.getElementById("modal-img");
+    let closeModal = document.getElementById("close-modal");
 
     // Abrir el modal cuando se haga clic en una imagen
     const images = document.querySelectorAll(".image img");
@@ -95,10 +87,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // CÓDIGO PARA ABRIR LAS RESPUESTAS DE LAS PREGUNTAS EN PREGUNTAS_Y_RESPUESTAS //
-
+  // CÓDIGO PARA ABRIR LAS RESPUESTAS DE LAS PREGUNTAS EN PREGUNTAS_Y_RESPUESTAS
   function initializeFAQ() {
-    faqItems = document.querySelectorAll('.faq-item');
+    let faqItems = document.querySelectorAll('.faq-item');
 
     faqItems.forEach(item => {
       const question = item.querySelector('.question');
@@ -122,8 +113,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
 
-    // Para cerrar las respuestas //
-
+    // Para cerrar las respuestas
     function closeAllQuestions() {
       faqItems.forEach(item => {
         const answerWrapper = item.querySelector('.answer-wrapper');
@@ -135,106 +125,9 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   }
-
-  /* CÓDIGO PARA INICIAR LA DURACIÓN DE LA TRANSICIÓN ENTRE PÁGINAS*/
-
-  // Función para introducir una pausa (delay) antes de completar una transición
-
-  function delay(n) {
-    n = n || 700; // Puedes ajustar la duración del retraso aquí
-    return new Promise((done) => {
-      setTimeout(() => {
-        done();
-      }, n);
-    });
-  }
-
-  // Función para obtener un color aleatorio de una lista de colores
-
-  function getRandomColor() {
-
-    /* const colors = ["#EC2127", "#EA0B8B", "#F58220", "#FFD200", "#77C043", "#2BABE2", "#724C9F", "#432F87"]; */
-
-    const colors = ["#B3000C", "#0D5901"];
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
-  }
-
-  // FUNCIÓN PARA MANEJAR LA TRANSICIÓN DE LA PÁGINA WEB //
-
-  function pageTransition() {
-    var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-      duration: 0.5,
-      width: "100%",
-      left: "0%",
-      ease: "Expo.easeInOut",
-      backgroundColor: getRandomColor(),
-      onComplete: function () {
-        gsap.to(".loading-image", { opacity: 1, duration: 0.1 });
-      }
-    });
-
-    tl.to(".loading-screen", {
-      duration: 0.5,
-      width: "100%",
-      left: "100%",
-      ease: "Expo.easeInOut",
-      delay: 0.2
-    });
-    tl.set(".loading-screen", { left: "-100%" });
-  }
-
-  // FUNCIÓN PARA ANIMAR EL CONTENIDO DE LA PÁGINA //
-
-  function contentAnimation() {
-    var tl = gsap.timeline();
-    tl.from(".animate-this", {
-      duration: 0.4,
-      y: 30,
-      opacity: 0,
-      stagger: 0.4,
-      delay: 0.2,
-      onComplete: function () {
-
-        // INICIAMOS LAS FUNCIONES DESPUÉS DE INICIAR LA TRANSICIÓN DE BARBA Y GSAP
-
-        initializeHamburguer();
-        initializeFAQ();
-        initializeModal();
-        initializeripples();
-      }
-    });
-  }
-
-  // EJECUTAMOS LA ANIMACIÓN DE LA PÁGINA //
-
-  pageTransition();
-
-  // CONFIGURACIÓN DE BARBA.JS //
-
-  barba.init({
-    sync: true,
-    transitions: [
-      {
-        async leave(data) {
-          const done = this.async();
-          pageTransition();
-          await delay(500);
-          done();
-        },
-
-        async enter(data) {
-          contentAnimation();
-        },
-
-        async once(data) {
-          contentAnimation();
-        },
-      },
-    ],
-  });
+  initializeFAQ(); // Llamada a la función después de que el DOM ha sido cargado
 });
+
 
 // CÓDIGO PARA DESGLOSAR OPCIONES DEL BOTÓN COMPRAR TICKETS DEL NAVBAR // 
 
@@ -410,20 +303,3 @@ window.addEventListener("click", function (event) {
     closeModal();
   }
 });
-
-
-/* POP UP */
-
-document.addEventListener("DOMContentLoaded", function () {
-  mostrarPopup();
-});
-
-function mostrarPopup() {
-  var popup = document.getElementById('popup');
-  popup.style.display = 'block';
-}
-
-function cerrarPopup() {
-  var popup = document.getElementById('popup');
-  popup.style.display = 'none';
-}
